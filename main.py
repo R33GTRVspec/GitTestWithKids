2f75572de73beb7c7a4ec3e6626de65e9b804855
Sergei Semenov <kocmohabt@me.com>
2023-02-12 15:24:56 +0300

    Два интерфейса в файлах.
    ui_mainwindow.py - это конвертация Сашиной версии дневника из файла ourschooldiary.ui
    
    mainwindow.py - это конвертация первоначального скетча программы со всем функционалом из файла ui_mainwindow.ui
    
    Если нужно поменять, то правим ui_mainwindow на mainwindow в первой части строки:
    from ui_mainwindow import Ui_MainWindow

--- main.py
+++ main.py
@@ -2,11 +2,14 @@
 
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
+from ui_mainwindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 
 if __name__ == '__main__':
@@ -15,4 +18,4 @@ if __name__ == '__main__':
     window = MainWindow()
     window.show()
 
-   sys.exit(app.exec())
+    sys.exit(app.exec())
