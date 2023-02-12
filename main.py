af5919a9c3012563bd04801b8265ba57a7e21516
Sergei Semenov <kocmohabt@me.com>
2023-02-12 14:48:05 +0300

    Переименовал ui файл в более удобный для гуглинга mainwindow.ui

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+# Загрузим созданный в дизайнере интерфейс
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
 
 
-# Press the green button in the gutter to run the script.
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication(sys.argv)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    window = MainWindow()
+    window.show()
+
+   sys.exit(app.exec())
